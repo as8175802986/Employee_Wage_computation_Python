7858a90d78846ed703ff4feda10822d8ade94a6e
“Arunesh <as8175802986@gmail.com>
2022-03-09 00:38:23 +0530

    [Arunesh]added code for check daily wage

--- check_daily_wage.py
+++ check_daily_wage.py
@@ -1,11 +1,16 @@
 import random
-def attendence():
+def daily_wage():
+    EMP_RATE_PER_HOUR = 20
     number = random.randint(0, 1)
     if number == 1:
         print("Employee is present")
+        empHrs = 8
     else:
         print("Employee is absent")
+        empHrs = 0
+    print("Daliy Wage is : ", empHrs * EMP_RATE_PER_HOUR)
 
 #Calling function to print result
 if __name__== '__main__':
-    attendence()
+    print(daily_wage())
+

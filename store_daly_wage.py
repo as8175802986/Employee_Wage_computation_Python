cd7e69d45036130f6b70c424f0db327b8d4b79ff
“Arunesh <as8175802986@gmail.com>
2022-03-11 03:32:33 +0530

    [Arunesh]added code for storing daily Wage

--- store_daly_wage.py
+++ store_daly_wage.py
@@ -1,4 +1,3 @@
-import random
 def fullTime():
     return 8
 def partTime():
@@ -6,6 +5,7 @@ def partTime():
 def absent():
     return 0
 def check_attendence():
+    import random
     number = random.randint(0, 2)
     switcher = {
         0: absent,
@@ -13,35 +13,25 @@ def check_attendence():
         2: partTime,
     }
     return switcher.get(number)
-def totalWage():
+def store_daily_wage():
     EMP_RATE_PER_HOUR = 20
-    NUM_OF_WORKING_DAYS = input("NUM_OF_WORKING_DAYS")
-    MAX_HRS_IN_MONTH = input("MAX_HRS_IN_MONTH")
+    NUM_OF_WORKING_DAYS = 20
+    MAX_HRS_IN_MONTH = 100
     totalEmpHrs = 0
     totalWorkingDays = 0
     totalEmpWage = 0
     empWage = []
-    while totalEmpHrs <= int(MAX_HRS_IN_MONTH) and totalWorkingDays < int(NUM_OF_WORKING_DAYS):
-
+    while totalEmpHrs <= int(MAX_HRS_IN_MONTH) and totalWorkingDays < NUM_OF_WORKING_DAYS:
         option = check_attendence()
         totalEmpHrs = totalEmpHrs + option()
-        empWage.append(option() * EMP_RATE_PER_HOUR )
+        empWage.append(option() * EMP_RATE_PER_HOUR)
         print("Daily wage : ", empWage[totalWorkingDays])
         totalWorkingDays += 1
-
     print(empWage)
     print("Total Employee Hours : ", totalEmpHrs)
     print("Total Employee Wage : ", sum(empWage))
 
-def check_Company():
-    number = random.randint(0, 1)
-    if number == 1 :
-     print("Bridgelabz")
-     totalWage()
 
-    else:
-     print("Wtvision")
-     totalWage()
-
+# print employee wage
 if __name__== '__main__':
-    check_Company()
+    store_daily_wage()
